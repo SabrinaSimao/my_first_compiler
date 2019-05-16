8e2d623bc144010aed00ad458a391f0803569e9c
SabrinaSimao <sabrina_simao@hotmail.com>
2019-05-16 10:22:47 -0300

    Fixed start label

--- Node.py
+++ Node.py
@@ -61,30 +61,15 @@ class UnOp(Node):
 
         f0 = self.children[0].Evaluate(ST)
         if self.value == '+':
-            if f0[1] == 'INTEGER':
                 Assembler.Write("MOV EAX, 1")
                 Assembler.Write("IMUL EBX")
                 Assembler.Write("MOV EBX, EAX")
-                #return (f0[0], 'INTEGER')
-            else:
-                raise ValueError("Unary operator + must be followed by an integer")
         elif self.value == '-':
-            if f0[1] == 'INTEGER':
                 Assembler.Write("MOV EAX, -1")
                 Assembler.Write("IMUL EBX")
                 Assembler.Write("MOV EBX, EAX")
-                #return (-f0[0], 'INTEGER')
-            else:
-                raise ValueError("Unary operator - must be followed by an integer")
         elif self.value == '~':
-            if f0[1] == 'BOOLEAN':
-                if f0[0] == 'TRUE':
-                    Assembler.Write("MOV EBX, False")
-                else:
-                    Assembler.Write("MOV EBX, True")
-                #return (not f0[0], 'BOOLEAN')
-            else:
-                raise ValueError("Unary operator ~ must be followed by an boolean")
+                Assembler.Write("NEG EBX")
         else:
             print("master blaster error")
 
@@ -106,7 +91,6 @@ class Print(Node):
 
     def Evaluate(self, ST):
         f = self.children[0].Evaluate(ST)
-        #print(f[0])
         Assembler.Write("PUSH EBX")
         Assembler.Write("CALL print")
         Assembler.Write("POP EBX")
@@ -124,68 +108,32 @@ class BinOp(Node):
         f1 = self.children[1].Evaluate(ST)
         Assembler.Write("POP EAX")
         if self.value == '+':
-            if (f0[1] == f1[1] and f0[1] == 'INTEGER'):
-                Assembler.Write("ADD EAX, EBX")
-                Assembler.Write("MOV EBX, EAX")
-                #return ([f0[0] + f1[0], 'INTEGER'])
-            else:
-                raise ValueError("Cant sum non integer values")
+            Assembler.Write("ADD EAX, EBX")
+            Assembler.Write("MOV EBX, EAX")
         elif self.value == '-':
-            if (f0[1] == f1[1] and f0[1] == 'INTEGER'):
-                Assembler.Write("SUB EAX, EBX")
-                Assembler.Write("MOV EBX, EAX")
-                #return ([f0[0] - f1[0], 'INTEGER'])
-            else:
-                raise ValueError("Cant subtract non integer values")
+            Assembler.Write("SUB EAX, EBX")
+            Assembler.Write("MOV EBX, EAX")
         elif self.value == '*':
-            if (f0[1] == f1[1] and f0[1] == 'INTEGER'):
-                Assembler.Write("IMUL EBX")
-                Assembler.Write("MOV EBX, EAX")
-                #return ([f0[0] * f1[0], 'INTEGER'])
-            else:
-                raise ValueError("Cant muktiply non integer values")
+            Assembler.Write("IMUL EBX")
+            Assembler.Write("MOV EBX, EAX")
         elif self.value == '/':
-            if (f0[1] == f1[1] and f0[1] == 'INTEGER'):
-                Assembler.Write("IDIV EBX")
-                Assembler.Write("MOV EBX, EAX")
-                #return ([f0[0] // f1[0], 'INTEGER'])
-            else:
-                raise ValueError("Cant divide non integer values")
+            Assembler.Write("IDIV EBX")
+            Assembler.Write("MOV EBX, EAX")
         elif self.value == '=':
-            if (f0[1] == f1[1]):
-                Assembler.Write("CMP EAX, EBX")
-                Assembler.Write("CALL binop_je")
-                #return ([f0[0] == f1[0], 'BOOLEAN'])
-            else:
-                raise ValueError("Cant compare values of diferent types")
+            Assembler.Write("CMP EAX, EBX")
+            Assembler.Write("CALL binop_je")
         elif self.value == '>':
-            if (f0[1] == f1[1] and f0[1] == 'INTEGER'):
-                Assembler.Write("CMP EAX, EBX")
-                Assembler.Write("CALL binop_jg")
-                #return ([f0[0] > f1[0], 'BOOLEAN'])
-            else:
-                raise ValueError("Cant compare (>) non integer values")
+            Assembler.Write("CMP EAX, EBX")
+            Assembler.Write("CALL binop_jg")
         elif self.value == '<':
-            if (f0[1] == f1[1] and f0[1] == 'INTEGER'):
-                Assembler.Write("CMP EAX, EBX")
-                Assembler.Write("CALL binop_jl")
-                #return ([f0[0] < f1[0], 'BOOLEAN'])
-            else:
-                raise ValueError("Cant compare (<) non integer values")
+            Assembler.Write("CMP EAX, EBX")
+            Assembler.Write("CALL binop_jl")
         elif self.value == 'OR':
-            if (f0[1] == f1[1] and f0[1] == 'BOOLEAN'):
-                Assembler.Write("OR EAX, EBX")
-                Assembler.Write("MOV EBX, EAX")
-                #return ([f0[0] or f1[0], 'BOOLEAN'])
-            else:
-                raise ValueError("Cant make OR with non boolean values")
+            Assembler.Write("OR EAX, EBX")
+            Assembler.Write("MOV EBX, EAX")
         elif self.value == 'AND':
-            if (f0[1] == f1[1] and f0[1] == 'BOOLEAN'):
-                Assembler.Write("AND EAX, EBX")
-                Assembler.Write("MOV EBX, EAX")
-                #return ([f0[0] and f1[0], 'BOOLEAN'])
-            else:
-                raise ValueError("Cant make AND with non boolean values")
+            Assembler.Write("AND EAX, EBX")
+            Assembler.Write("MOV EBX, EAX")
         else:
             print("master blaster error")
 
@@ -197,7 +145,6 @@ class IntVal(Node):
     
     def Evaluate(self, ST):
         Assembler.Write("MOV EBX, {}".format(self.value))
-        #return [int(self.value), 'INTEGER']
         
 
 class BoolVal(Node):
@@ -208,7 +155,6 @@ class BoolVal(Node):
     
     def Evaluate(self, ST):
         Assembler.Write("MOV EBX, {}".format(self.value))
-        #return [self.value, 'BOOLEAN']
 
 class Identifier(Node):
     def __init__(self, value, children):
@@ -218,10 +164,7 @@ class Identifier(Node):
     
     def Evaluate(self, ST):
         #colocar shift do getter da ST
-        Assembler.Write("MOV EBX, [EBP-{}]".format(ST.getter(self.children[0].value)[2]))
-        #value = ST.getter(self.value)
-        #return value
-
+        Assembler.Write("MOV EBX, [EBP-{}]".format(ST.getter(self.value)[2]))
 
 class Input(Node):
     def __init__(self, value, children):
@@ -261,12 +204,9 @@ class IF(Node):
         Assembler.Write("JMP EXIT_{}:".format(self.id))
         Assembler.Write("if_{}:".format(self.id))
 
-        #if(condition[0] == 'TRUE' or condition[0] == True):
         #if condition is always evaluated
         for f in self.children[1]:
             f.Evaluate(ST)
-        #else:
-        #    pass
 
         Assembler.Write("EXIT_{}:".format(self.id))
 
@@ -282,7 +222,6 @@ class WHILE(Node):
         
         Assembler.Write("CMP EBX, False")
         Assembler.Write("JE EXIT_{}:".format(self.id))
-        #while (self.children[0].Evaluate(ST)[0] == True) or (self.children[0].Evaluate(ST)[0] == 'TRUE'):
         for f in self.children[1]:
             f.Evaluate(ST)
         Assembler.Write("JMP LOOP_{}:".format(self.id))
